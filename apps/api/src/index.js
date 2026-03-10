ca2d11acdd162a40b97c203f3144bedf58f5d8c9
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-10 15:53:23 +0500

    feat: add `/chars` route & render

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,13 +1,25 @@
 import fastify from "fastify"
+import cors from "@fastify/cors"
 
 const PORT = parseInt( process.env.API_PORT || 3_000 )
 const app = fastify( { logger: true } )
+
+app.register( cors, {
+	origin: "*",
+	methods: [ "GET", "POST", "PATCH", "PUT", "DELETE" ],
+	credentials: true,
+} )
 
 app.get( "/", () => {
 
 	return {
 		ok: true,
 	}
+} )
+
+app.get( "/chars", () => {
+
+	return [ "A", "B", "C", "D", "E", "F", "G" ]
 } )
 
 app.listen( { port: PORT } )
--- apps/web/src/main.js
+++ apps/web/src/main.js
@@ -1,1 +1,17 @@
-console.log( "Web" )
+import "./main.css"
+
+const charsUL = document.getElementById( "chars" )
+
+fetch( "http://localhost:3000/chars" )
+.then( response => response.json() )
+.then( characters => {
+
+	for ( const char of characters ) {
+
+		const li = document.createElement( "LI" )
+
+		li.textContent = char
+
+		charsUL.appendChild( li )
+	}
+} )
